ca6d06cf7e1fe689eae4b7e3001300eb98ae292e
0ev <andyye.jongcye@gmail.com>
2022-05-23 02:05:00 +0900

    Feat add account db and authorization

--- seminar/src/middleware/auth.js
+++ seminar/src/middleware/auth.js
@@ -1,8 +1,50 @@
-const authMiddleware = (req, res, next) => {
-  if (
-    req.body.credential === process.env.API_KEY &&
-    req.body.password === process.env.API_PASSWORD
-  ) {
+const AccountModel = require("../models/account");
+
+class AccountDB {
+  static _inst_;
+  static getInst = () => {
+    if (!AccountDB._inst_) AccountDB._inst_ = new AccountDB();
+    return AccountDB._inst_;
+  };
+
+  #total = 10000;
+
+  constructor() {
+    try {
+      const newItem = new AccountModel({
+        key: process.env.API_KEY,
+        password: process.env.API_PASSWORD,
+      });
+      const res = newItem.save();
+    } catch (e) {
+      console.log(`[Account-DB] Insert Error: ${e}`);
+    }
+    console.log("[Account-DB] DB Init Completed");
+  }
+
+  authorize = async (credential, password) => {
+    const res = await AccountModel.findOne(
+      { key: credential, password: password },
+      (err, doc) => {
+        if (err) return false;
+        if (doc) {
+          return true;
+        }
+        return false;
+      }
+    ).clone();
+    return res;
+  };
+}
+
+const AccountDBInst = AccountDB.getInst();
+
+const authMiddleware = async (req, res, next) => {
+  const isOK = await AccountDBInst.authorize(
+    req.body.credential,
+    req.body.password
+  );
+  if (isOK) {
     console.log("[AUTH-MIDDLEWARE] Authorized User");
     next();
   } else {
